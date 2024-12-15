cf37f0d04a9aa8eb8f62b35277fa283e24053b3e
anaximeno <anaximenobrito@gmail.com>
2024-12-15 16:49:20 -0100

    Add game play consumer

--- backend/GameServer/consumers.py
+++ backend/GameServer/consumers.py
@@ -4,6 +4,8 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class GameServerConsumer(WebsocketConsumer):
+    """Serves game info."""
+
     def connect(self):
         self.accept()
 
@@ -14,3 +16,16 @@ class GameServerConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({"message": "Received!"}))
         return super().receive(text_data, bytes_data)
 
+
+class GamePlayConsumer(WebsocketConsumer):
+    """Sends and receive playing time data."""
+
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, code):
+        return super().disconnect(code)
+
+    def receive(self, text_data=None, bytes_data=None):
+        self.send(text_data=json.dumps({"message": "Received!"}))
+        return super().receive(text_data, bytes_data)
--- backend/GameServer/routing.py
+++ backend/GameServer/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .consumers import GameServerConsumer
+from .consumers import GameServerConsumer, GamePlayConsumer
 
 
 wbwebsocket_urlpatterns = [
-    re_path(r'ws/game/server/$', GameServerConsumer.as_asgi()),
+    re_path(r"ws/game/server/$", GameServerConsumer.as_asgi()),
+    re_path(r"ws/game/play/$", GamePlayConsumer.as_asgi()),
 ]
